0eb56d28d259eb1312eb47b87edadc7a23f39a73
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-05-22 13:32:21 -0700

    Find borough with the largest standard deviation in combined SAT score

--- main.py
+++ main.py
@@ -50,3 +50,30 @@ schools_total_sat_srt = schools.loc[:, cols_to_subset_total_sat].sort_values("to
 
 # Find top 10 SAT score schools
 top_10_schools = schools_total_sat_srt.head(10)
+
+# -------------------------------------------------------------------------------------------------------------------
+# QUESTION 3: Which single borough has the largest standard deviation in the combined SAT score?
+
+# Find the standard deviation in boroughs
+std_boroughs = schools.groupby("borough")["total_SAT"].std().round(2).reset_index()
+
+# Rename the "total SAT column" to "std_total_SAT"
+std_boroughs = std_boroughs.rename(columns={"total_SAT": "std_SAT"})
+
+# Sort the results in descending order to find the total SAT highest standard deviation
+largest_std_dev = std_boroughs.sort_values("std_SAT", ascending=False).head(1)
+
+# Find the name of the borough
+largest_std_dev_borough = largest_std_dev["borough"].iloc[0]
+
+# Filter the schools in the total SAT highest standard deviation borough
+borough_schools = schools[schools["borough"] == largest_std_dev_borough]
+
+# Find the number of schools in the borough
+number_of_borough_schools = borough_schools.shape[0]
+
+# Set the number of schools of the total SAT highest standard deviation
+largest_std_dev["num_schools"] = number_of_borough_schools
+
+# Find the mean of "total_SAT" in the borough and set it to the new column
+largest_std_dev["average_SAT"] = borough_schools["total_SAT"].mean().round(2)
